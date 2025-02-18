8f95dea2870ba6a34ed36408047375f8843278bd
Agostino Ruscito <ruscito@users.noreply.github.com>
2025-02-18 13:24:45 -0600

    Create asim.h

---
+++ asim.h
@@ -0,0 +1,15 @@
+/* asim.h */
+#ifndef _ASIM_H_
+#define _ASIM_H_
+
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <assert.h>
+#include <errno.h>
+
+
+#endif // _ASIM_H_
